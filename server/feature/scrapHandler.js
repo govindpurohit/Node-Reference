d8430e4c16b799f1502faee17b733ca0c8678c4d
Govind Purohit <govind.rajpurohit@stackpathtech.com>
2017-05-09 12:28:29 +0530

    node-modules removed

--- server/feature/scrapHandler.js
+++ server/feature/scrapHandler.js
@@ -1,7 +1,27 @@
 var schedule = require('node-schedule');
 
+const USER = require('../middlewares/users');
 var scrapper = require('./newsScrapper');
+const EXP = require('../middlewares/expression');
 
 var j = schedule.scheduleJob('*/10 * * * * *', function(){
   console.log('The answer to life, the universe, and everything!');
 });
+
+exports.liveNews = function(){
+  let allUsers = getAllUser();
+  if(allUsers != null){
+    get
+  }
+}
+
+function getAllUser(){
+  let users;
+  USER.getAllUsers().then((data) => {
+    users = (data && data.length > 0) ? data : null;
+  },
+  (err) => {
+    console.log("Error:"+err);
+  });
+  return users;
+}
